61b540b3cb1c78eb8d7ea8deb840e8f5ccf067ee
DanielleAgat <agatlevi96@gmail.com>
2021-04-01 15:26:32 +0300

    working on meanHNeap

--- MinHeap.cpp
+++ MinHeap.cpp
@@ -1,5 +1,145 @@
-//
-// Created by Aviv Nevelev on 01/04/2021.
-//
-
 #include "MinHeap.h"
+
+namespace Graph{
+    MinHeap::MinHeap(int _phySize):phySize(_phySize){
+        heapData = new HoffmanTree[phySize];
+        logSize = 0;
+        isAllocated = true;
+    }
+
+    MinHeap::MinHeap(int arr[],int size) {
+        logSize = phySize = size;
+        heapData = arr;
+        isAllocated = false;
+
+        for(int i = size/2-1 ; i >= 0 ; i--)
+            fixHeap(i);
+    }
+
+    MinHeap::MinHeap(const MinHeap &toCopy) {
+        *this=toCopy;
+    }
+
+    MinHeap &MinHeap::operator=(const MinHeap &toCopy) {
+        if (this != &toCopy) {
+            if(isEmpty()){
+                makeEmpty();
+            }
+            logSize = toCopy.logSize;
+            isAllocated = toCopy.isAllocated;
+            phySize = toCopy.phySize;
+            HoffmanTree *tmp = new HoffmanTree[toCopy.phySize];
+            for (int i = 0; i < logSize; i++) {
+                tmp[i] = toCopy.heapData[i];
+            }
+            heapData = tmp;
+        }
+        return *this;
+    }
+    MinHeap::~MinHeap(){
+        if(isAllocated)
+            delete[] heapData;
+        heapData = nullptr;
+    }
+
+    int MinHeap::left(int nodeIndex){
+        return (2*nodeIndex + 1);
+    }
+    int MinHeap::right(int nodeIndex){
+        return (2*nodeIndex + 2);
+    }
+    int MinHeap::parent(int nodeIndex){
+        return (nodeIndex-1)/2;
+    }
+
+    void MinHeap::fixHeap(int nodeIndex) {
+        int min;
+        int _left = left(nodeIndex);
+        int _right = right(nodeIndex);
+
+        if ((_left < logSize) && (heapData[_left].getRootKey() < heapData[nodeIndex].getRootKey())) {
+            min = _left;
+        } else {
+            min = nodeIndex;
+        }
+        if ((_right < logSize) && (heapData[_right].getRootKey() < heapData[min].getRootKey()))
+            min = _right;
+        //Swap values if necessary and continue fixing the heap towards the leaves:
+        if (min != nodeIndex) {
+            swap(heapData[nodeIndex], heapData[min]);
+            fixHeap(min);
+        }
+    }
+
+    HoffmanTree* MinHeap::deleteMin() {
+        if(logSize < 1){
+            cout << "invalid input" << endl;
+            exit(1);
+        }else{
+            HoffmanTree* min = new HoffmanTree();
+            *min = *heapData;
+            logSize--;
+            heapData[0] = heapData[logSize];
+            fixHeap(0);
+            return min;
+        }
+    }
+
+    void MinHeap::insert(HoffmanTree item) {
+        if(logSize == phySize){
+            cout << "invalid input" << endl;
+            exit(1);
+        }
+        int i = logSize;
+        logSize++;
+
+        while((i > 0) && (heapData[parent(i)].getRootKey() > item.getRootKey())){
+            heapData[i] = heapData[parent(i)];
+            i = parent(i);
+        }
+        heapData[i] = item;
+    }
+
+    void MinHeap::makeHeap(BinSearchNode* node){
+        Pair* heapArr=node->makeDataArr(phySize,logSize);
+        for(int i=0;i<logSize;i++){
+            HoffmanTree HoffmanTree;
+            HoffmanTree.insert(heapArr[i].frequency, heapArr[i].key);
+            heapData[i]=HoffmanTree;
+        }
+        for(int i=logSize/2-1;i>=0;i--){ // floyd.
+            fixHeap(i);
+        }
+    }
+
+
+    bool MinHeap::isEmpty() const {
+        if (logSize == 0)
+            return true;
+        return false;
+    }
+
+    void MinHeap::makeEmpty() {
+        logSize=0;
+    }
+
+    HoffmanTree* MinHeap::min() {
+        if (logSize < 1) {
+            cout << "invalid input" << endl;
+            exit(1);
+        } else {
+            HoffmanTree *min = new HoffmanTree();
+            *min = *heapData;
+            return min;
+        }
+    }
+
+
+
+    void swap(HoffmanTree& x, HoffmanTree& y){
+        HoffmanTree temp = x;
+        x = y;
+        y = temp;
+    }
+
+}
--- MinHeap.h
+++ MinHeap.h
@@ -1,8 +1,53 @@
-//
-// Created by Aviv Nevelev on 01/04/2021.
-//
+//Student Name: Danielle Agat Levi
+//Student ID: 313357329
+//Student Name: Aviv Nevelev
+//Student ID: 206026429
+#pragma once
 
-#ifndef ALGOPROJECT_MINHEAP_H
-#define ALGOPROJECT_MINHEAP_H
+namespace Graph{
+    class MinHeap{
+    private:
+        int heapData;
+        int phySize;
+        int logSize;
+        bool isAllocated; // 'true' if heap allocated memory ; 'false' otherwise.
 
-#endif //ALGOPROJECT_MINHEAP_H
+        ///Inner Methods:
+        // calc leftChild index
+        static int left(int nodeIndex);
+        // calc rightChild index
+        static int right(int nodeIndex);
+        // calc parent index
+        static int parent(int nodeIndex);
+        //fixHeap based on algorithm learned in class.
+        void fixHeap(int nodeIndex);
+
+    public:
+        ///Constructors & Destructors:
+        //Allocate memory for the heap and make the heap empty.
+        MinHeap(int _phySize);
+        //copy ctor
+        MinHeap(const MinHeap& toCopy);
+        //turns arr of frequencies and chars into min heap.
+        MinHeap(int arr[],int size);
+        //destructor
+        ~MinHeap();
+
+        ///Methods:
+        //return min without delete.
+        int min();
+        // reset logSize
+        void makeEmpty();
+        //check if empty
+        bool isEmpty() const;
+        // return and delete min
+        int deleteMin();
+        // insert HoffmanTree to heap
+        void insert(int item);
+
+        ///Overriding operators:
+        MinHeap& operator=(const MinHeap& toCopy);
+    };
+    //swap 2 integers
+    void swap(int& x, int& y);
+}
