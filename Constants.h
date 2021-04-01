018ffb910886194c2b40540da201c98d09c734a1
Aviv Nevelev <aviv.nevelev@gmail.com>
2021-04-01 11:41:45 +0300

    added Constants.h

---
+++ Constants.h
@@ -0,0 +1,15 @@
+//
+// Created by Aviv Nevelev on 01/04/2021.
+//
+
+#ifndef ALGOPROJECT_CONSTANTS_H
+
+#define PROTECTED
+#define PUBLIC
+#define PRIVATE
+#define FALSE 0
+#define TRUE 1
+#define MAX_LINE_LEN 256
+#define ALGOPROJECT_CONSTANTS_H
+
+#endif //ALGOPROJECT_CONSTANTS_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "Constants.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc,char** argv) {
+
+
+
     return 0;
 }
