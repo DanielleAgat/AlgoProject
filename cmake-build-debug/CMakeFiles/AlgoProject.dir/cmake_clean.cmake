d200c44e14b8c2e56d34fea4ae610c8e42029d13
DanielleAgat <agatlevi96@gmail.com>
2021-04-01 13:10:39 +0300

    removed gitIgmore

---
+++ cmake-build-debug/CMakeFiles/AlgoProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AlgoProject"
+  "AlgoProject.pdb"
+  "CMakeFiles/AlgoProject.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgoProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
