ecef3b8f299a784f85fdfd1ab11c8ce23283e5b3
DanielleAgat <agatlevi96@gmail.com>
2021-04-01 12:30:13 +0300

    finished readFile functions

--- main.cpp
+++ main.cpp
@@ -4,17 +4,23 @@
 #include <list>
 
 using namespace std;
+list<arc> readFile(int, char**, int&, int&, int&);
+
 
 int main(int argc,char** argv) {
+    int n; //num of vertexes
+    int s; //start vertex
+    int t; //destination vertex
+    list<arc> arcs; //List of arcs
+    arcs = readFile(argc,argv,n,s,t);
+
+    
 
     return 0;
 }
 
 
-void readFile(int argc, char** argv){
-    int n; //num of vertexes
-    int s; //start vertex
-    int t; //destination vertex
+list<arc> readFile(int argc, char** argv,int& n, int& s, int& t){
     ifstream file;
     string fileName = argv[1];
     fileName += ".txt";
@@ -25,7 +31,6 @@ void readFile(int argc, char** argv){
     }
 
     file >> n >> s >> t;
-
     list<arc> arcs;
     arc currArc;
     file >> currArc.i >> currArc.j >> currArc.weight;
@@ -39,6 +44,6 @@ void readFile(int argc, char** argv){
         arcs.push_back(currArc);
     }
     file.close();
-    cout << endl;
+    return arcs;
 }
 
