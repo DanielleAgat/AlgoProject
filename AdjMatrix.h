15b3a6db7caf8cf34be43e65e4370b4f6aaccb09
DanielleAgat <agatlevi96@gmail.com>
2021-04-01 14:11:49 +0300

    Created files for AdjMatrix and AdjList classes

--- AdjMatrix.h
+++ AdjMatrix.h
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <list> //TODO: implement our own list
 #pragma once
 
+using namespace std;
 
 class AdjMatrix {
-
+private:
+    int** matrix;
+public:
+    AdjMatrix(int n);
+    void MakeEmptyGraph(int n);
+    bool IsAdjacent(int u, int v);
+    list<int> GetAdjList(int u);
+    bool AddEdge(int u, int v, int c);
+    bool RemoveEdge(int u, int v);
 };
